3e70b9fe07265c35abb09f6fdfb6ec2f6b36e209
Xabier Andres <xabier_andres@hotmail.com>
2018-04-23 22:18:42 +0200

    Initial Commit

---
+++ FooPin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FooPin
+//
+//  Created by Xabier Andrés Irulegui on 23/4/18.
+//  Copyright © 2018 Xabier Andrés Irulegui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
